321806956576cec9c452b65e810c0bb84b27a909
151090 <m.helmy.amit@gmail.com>
2021-02-26 15:28:38 +0200

    test upload more than file

---
+++ main (2).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     char temp = 22 ;
+    if(temp == 0)
+    {
+    printf("%d",temp);
+    }
+    else
+    printf("test");
+
+    return 0;
+}
